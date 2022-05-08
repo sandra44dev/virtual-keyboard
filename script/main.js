08a480d42e643358bdcdb2574905d13dc38e502b
Alexandra Andreeva <sandra44dev@gmail.com>
2022-05-08 18:04:12 +0400

    feat: add symbols print

--- script/main.js
+++ script/main.js
@@ -1,6 +1,7 @@
-import { FIRST_LINE_SYMBOLS } from './symbols.js';
+'use strict';
 
-document.body.onload = addMarkup;
+import { SYMBOLS } from './symbols.js';
+
 function addMarkup() {
     const WRAPPER = document.createElement("div");
     document.body.appendChild(WRAPPER);
@@ -9,36 +10,80 @@ function addMarkup() {
     const TEXTAREA = document.createElement("textarea");
     WRAPPER.appendChild(TEXTAREA);
     TEXTAREA.setAttribute('placeholder', 'time to type something...')
+    TEXTAREA.classList.add('textarea');
 
     const KEYBOARD = document.createElement("div");
     WRAPPER.appendChild(KEYBOARD);
     KEYBOARD.classList.add('keyboard');
 
+    const INFO = document.createElement("p");
+    WRAPPER.appendChild(INFO);
+    INFO.classList.add('info');
+    INFO.innerHTML = 'alt + space to change the language<br><br>developed on Windows OS';
+
     let i;
 
     class Key {
         constructor(symbols) {
-            this.symbol = symbols[`${i}`].default;
-            this.HTML = '<div class = "key"></div>'
+            if (symbols[`${i}`].default !== undefined) {
+                this.symbol = symbols[`${i}`].default;
+            } else {
+                this.symbol = symbols[`${i}`].en;
+            }
         }
 
     }
 
-    for (i = 0; i < FIRST_LINE_SYMBOLS.length; i++) {
+    for (i = 0; i < SYMBOLS.length; i++) {
 
-        let x = new Key(FIRST_LINE_SYMBOLS);
+        let newButton = new Key(SYMBOLS);
 
         let newKey = document.createElement('div');
         KEYBOARD.appendChild(newKey);
-        newKey.classList.add('key');
-        newKey.innerHTML = x.symbol;
+        newKey.innerHTML = newButton.symbol;
 
-        console.log(x.symbol);
-
-        if (x.symbol == '`' || x.symbol == 'backspace') {
+        if (newButton.symbol == '`' || newButton.symbol == 'backspace' || newButton.symbol == 'tab' || newButton.symbol == '\\' || newButton.symbol == 'enter' || newButton.symbol == 'caps lock' || newButton.symbol == 'ctrl' || newButton.symbol == 'alt') {
             newKey.classList.add('side-key');
-        }
-
+        } else if (newButton.symbol == 'shift') {
+            newKey.classList.add('side-key');
+            newKey.classList.add('shift');
+        } else if (newButton.symbol == ' ') {
+            newKey.classList.add('key');
+            newKey.classList.add('side-key');
+            newKey.classList.add('space');
+        } else { newKey.classList.add('key'); }
     }
 }
 
+function addPrintFunc() {
+    const KEYS = document.querySelectorAll('.key');
+    console.log(KEYS)
+    const TEXTAREA = document.querySelector('.textarea');
+
+    function printSymbol(letterKey) {
+        TEXTAREA.focus();
+        let letter = letterKey.currentTarget.innerHTML;
+
+        let cursorStart = TEXTAREA.selectionStart;
+        let cursorEnd = TEXTAREA.selectionEnd;
+        let currentText = TEXTAREA.value;
+
+        let stringStart = currentText.substring(0, cursorStart);
+        let stringEnd = currentText.substring(cursorEnd, currentText.length);
+
+        TEXTAREA.value = stringStart + letter + stringEnd;
+
+        cursorStart++;
+        cursorEnd = cursorStart;
+        TEXTAREA.setSelectionRange(cursorStart, cursorEnd)
+        TEXTAREA.focus();
+        console.log('hi')
+    }
+
+    KEYS.forEach(item => {
+        item.addEventListener('click', printSymbol)
+    });
+}
+addMarkup();
+addPrintFunc();
+//document.body.onload = addPrintFunc;
--- script/symbols.js
+++ script/symbols.js
@@ -1,4 +1,4 @@
-const FIRST_LINE_SYMBOLS = Array.from([
+const SYMBOLS = Array.from([
     {
         'default': '`',
         'shift': '~',
@@ -54,17 +54,186 @@ const FIRST_LINE_SYMBOLS = Array.from([
     },
     {
         'default': 'backspace',
+    },  // second line
+    {
+        'default': 'tab',
     },
+    {
+        'en': 'q',
+        'ru': 'й',
+    },
+    {
+        'en': 'w',
+        'ru': 'ц',
+    },
+    {
+        'en': 'e',
+        'ru': 'у',
+    },
+    {
+        'en': 'r',
+        'ru': 'к',
+    },
+    {
+        'en': 't',
+        'ru': 'е',
+    },
+    {
+        'en': 'y',
+        'ru': 'н',
+    },
+    {
+        'en': 'u',
+        'ru': 'г',
+    },
+    {
+        'en': 'i',
+        'ru': 'ш',
+    },
+    {
+        'en': 'o',
+        'ru': 'щ',
+    },
+    {
+        'en': 'p',
+        'ru': 'з',
+    },
+    {
+        'en': '[',
+        'ru': 'х',
+    },
+    {
+        'en': ']',
+        'ru': 'ъ',
+    },
+    {
+        'en': '\\',
+        'ru': '\\',
+    },  // third line
+    {
+        'default': 'caps lock',
+    },
+    {
+        'en': 'a',
+        'ru': 'ф',
+    },
+    {
+        'en': 's',
+        'ru': 'ы',
+    },
+    {
+        'en': 'd',
+        'ru': 'в',
+    },
+    {
+        'en': 'f',
+        'ru': 'а',
+    },
+    {
+        'en': 'g',
+        'ru': 'п',
+    },
+    {
+        'en': 'h',
+        'ru': 'р',
+    },
+    {
+        'en': 'j',
+        'ru': 'о',
+    },
+    {
+        'en': 'k',
+        'ru': 'л',
+    },
+    {
+        'en': 'l',
+        'ru': 'д',
+    },
+    {
+        'en': ';',
+        'ru': 'ж',
+    },
+    {
+        'en': "'",
+        'ru': 'э',
+    },
+    {
+        'default': 'enter',
+    },  // forth line
+    {
+        'default': 'shift',
+    },
+    {
+        'en': 'z',
+        'ru': 'я',
+    },
+    {
+        'en': 'x',
+        'ru': 'ч',
+    },
+    {
+        'en': 'c',
+        'ru': 'с',
+    },
+    {
+        'en': 'v',
+        'ru': 'м',
+    },
+    {
+        'en': 'b',
+        'ru': 'и',
+    },
+    {
+        'en': 'n',
+        'ru': 'т',
+    },
+    {
+        'en': 'm',
+        'ru': 'ь',
+    },
+    {
+        'en': ',',
+        'ru': 'б',
+    },
+    {
+        'en': '.',
+        'ru': 'ю',
+    },
+    {
+        'en': '/',
+        'ru': '.',
+    },
+    {
+        'default': '⮙',
+    },
+    {
+        'default': 'shift',
+    },  // fifth line
+    {
+        'default': 'ctrl',
+    },
+    {
+        'default': 'alt',
+    },
+    {
+        'default': ' ',
+    },
+    {
+        'default': 'alt',
+    },
+    {
+        'default': '⮘',
+    },
+    {
+        'default': '⮛',
+    },
+    {
+        'default': '⮚',
+    },
+    {
+        'default': 'ctrl',
+    },
+
 ])
 
-// const SECOND_LINE_SYMBOLS = Array.from([
-//     {
-//         'default': 'tab',
-//     },
-//     {
-//         'en': 'q',
-//         'ru': 'й',
-//     },
-// ])
-
-export { FIRST_LINE_SYMBOLS }; // список экспортируемых переменных
+export { SYMBOLS }; // список экспортируемых переменных
